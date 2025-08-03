f76cbc838167cb26ab9b676d9e5bb8774e9a2103
Waleed Alfar <waleedalfar1024@gmail.com>
2025-08-03 15:21:46 -0700

    initial structure created

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import find_packages, setup 
 
 setup(
-    name="firstresponse-ai",
-    version="0.0.1",
-    author="Waleed Alfar",
-    author_email="contact@waleedalfar.com"
-    packages= find_packages(),
-    install_requires=[]
+    name = 'firstresponse-ai',
+    version ='0.0.1',
+    author ='Waleed Alfar',
+    author_email ='contact@waleedalfar.com',
+    packages = find_packages(),
+    install_requires = []
 
 )
